19dffcbc1da1b7edc7189c0c568aa779abd61739
AWSafran <andrewsafran@gmail.com>
2019-07-11 16:05:06 -0400

    added torch and room darkness functionality

--- src/adv.py
+++ src/adv.py
@@ -6,21 +6,21 @@ from item import Item
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", False),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", False),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", False),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", False),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", True),
 }
 
 # Declare Items
@@ -28,12 +28,14 @@ earlier adventurers. The only exit is to the south."""),
 item = {
     'sword': Item("sword", "An old sword that doesn't look very strong"),
     'key': Item("key", "A shiny gold Key"),
-    'book': Item("book", "A dusty book written in another language. You can't read any of it")
+    'book': Item("book", "A dusty book written in another language. You can't read any of it"),
+    'torch': Item("torch", "A burning torch hanging on the wall")
 }
 
 room['foyer'].add_item(item['book'])
 room['narrow'].add_item(item['key'])
 room['narrow'].add_item(item['sword'])
+room['overlook'].add_item(item['torch'])
 
 
 # Link rooms together
@@ -61,6 +63,7 @@ while True:
     
     
     cmd = input("\nPlease enter a command: \n")
+    cmd = cmd.lower()
 
     if cmd == 'q':
         break
--- src/player.py
+++ src/player.py
@@ -6,22 +6,37 @@ class Player:
         self.name = name
         self.room = room
         self.inventory = []
+        self.has_torch = False
 
     def take_item(self, item):
         item.on_take()
         self.inventory.append(item)
         self.room.take_item(item)
+        if item.name == "torch":
+            self.has_torch = True
+        
 
     def drop_item(self, item):
         print(f"You dropped your {item.name}")
         self.inventory.remove(item)
         self.room.add_item(item)
+        if item.name == "torch":
+            self.has_torch = False
 
     def enter_room(self, room):
         self.room = room
         print(f"You arrive in the {room.name}")
-        print(room.description)
-        room.print_items()
+        if not self.can_see():
+            print(f"It's too dark to see in here")
+        else:
+            print(room.description)
+            room.print_items()
+
+    def can_see(self):
+        if self.room.is_dark and not self.has_torch:
+            return False
+        else:
+            return True
 
     def print_items(self):
         if len(self.inventory) == 0:
@@ -38,7 +53,9 @@ class Player:
                 if item.name == command[1]:
                     pickup_item = item
                     break
-            if pickup_item is None:
+            if not self.can_see():
+                print("It's way too dark to do that")
+            elif pickup_item is None:
                 print("That item is not in this room")
             else:
                 self.take_item(pickup_item)
